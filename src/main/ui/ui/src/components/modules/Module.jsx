8d8d8389c63371830ff4565514d1ca9fd4c5ad8b
ErkanChalmers <sanneer@student.chalmers.se>
2021-04-21 20:33:12 +0200

    Vertically center content in modules

--- src/main/ui/ui/src/components/modules/Module.jsx
+++ src/main/ui/ui/src/components/modules/Module.jsx
@@ -6,11 +6,26 @@ const Module = ({ title, style, className, children }) => {
     return (
         <div className={ 'module ' + className } style={style}>
             { title && <h3> {title} </h3>}
-            <ErrorBoundary>
-                { children }
-            </ErrorBoundary>
+            <div style={outerContainer}>
+                <div style={innerContainer}>
+                    <ErrorBoundary>
+                        { children }
+                    </ErrorBoundary>
+                </div>
+            </div>
         </div>
     );
+}
+
+const outerContainer = {
+    height: 'calc(100% + max(-16vw, -96px))',
+    justifyContent: 'center',
+    display: 'flex',
+    flexDirection: 'column'
+}
+
+const innerContainer = {
+    display: 'block'
 }
 
 export default Module;
