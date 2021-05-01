1ff1ebab0fef9eef6891d6581364944b47aa7f28
ErkanChalmers <sanneer@student.chalmers.se>
2021-05-01 16:41:52 +0200

    New splash screen

--- src/main/ui/ui/src/components/Splash.jsx
+++ src/main/ui/ui/src/components/Splash.jsx
@@ -1,16 +1,23 @@
 import '../styles/App.css';
 import React from 'react';
-import Spinner from 'react-bootstrap/Spinner';
 
 const Splash = () => {
     return (
         <section className={ 'page-wrapper splash' }>
-            <div style={{textAlign: 'center'}}>
-                <h2>Welcome</h2>
-                <Spinner animation="grow" />
+            <div className={'content-wrapper'} style={{textAlign: 'center'}}>
+                <div className={'text-wrapper'}>
+                    <h2 className={'glitch'} data-text={"GAINZ TRACKER"}>GAINZ TRACKER</h2>
+                </div>
             </div>
+            <p style={footerText}>© Erik Sänne | 2020 </p>
         </section>
     );
+}
+
+const footerText = {
+    position: 'absolute',
+    bottom: '0px',
+    margin: '5px'
 }
 
 export default Splash;
