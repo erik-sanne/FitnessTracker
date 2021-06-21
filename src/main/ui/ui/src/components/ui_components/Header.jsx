a1eb209d695795683565cbbca224e9e823b25c84
ErkanChalmers <sanneer@student.chalmers.se>
2021-06-21 21:26:40 +0200

    Escape hatch on header text

--- src/main/ui/ui/src/components/ui_components/Header.jsx
+++ src/main/ui/ui/src/components/ui_components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 const Header = ({ title }) => {
     return (
         <header>
-            <h2 className={ 'header' }>
-                { title }
-            </h2>
+                <h2 className={ 'header' }>
+                    <NavLink to="/general" style={{ color: 'inherit', textDecoration: 'none'}}>
+                       { title }
+                    </NavLink>
+                </h2>
         </header>
     );
 }
