3518d35a497874c6677ed90f504d366541fd28cb
ErkanChalmers <sanneer@student.chalmers.se>
2022-01-17 02:15:38 +0100

    new achievement

---
+++ src/main/java/com/ersa/tracker/services/general/achivements/CappedCaptainAchievement.java
@@ -0,0 +1,35 @@
+package com.ersa.tracker.services.general.achivements;
+
+import com.ersa.tracker.models.authentication.User;
+import com.ersa.tracker.services.general.WorkoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CappedCaptainAchievement extends AchievementProviderBase {
+
+    @Autowired
+    WorkoutService workoutService;
+
+    @Override
+    public String getName() {
+        return "Captain Capped";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Equipped with death star deltoids, Captain Capped has targeted all three heads of the deltoids 100 times each";
+    }
+
+    @Override
+    public boolean evaluate(User user) {
+        boolean front =
+                workoutService.getPartitionedWorkoutSets(user, "MILITARY_PRESS").size() +
+                workoutService.getPartitionedWorkoutSets(user, "SHOULDER_PRESS").size() > 100;
+        boolean side = workoutService.getPartitionedWorkoutSets(user, "LATERAL_RAISES").size() > 100;
+        boolean rear = workoutService.getPartitionedWorkoutSets(user, "REVERSED_FLIES").size() > 100;
+
+        return front && side && rear;
+    }
+}
