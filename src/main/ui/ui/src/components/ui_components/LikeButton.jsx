ba185d2f9273e82779fef501b767f1e468057ceb
ErkanChalmers <sanneer@student.chalmers.se>
2022-02-08 00:01:44 +0100

    Minor layout fix

--- src/main/ui/ui/src/components/ui_components/LikeButton.jsx
+++ src/main/ui/ui/src/components/ui_components/LikeButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const LikeButton = ({ count, onClick }) => {
     return (
         <div className={ 'like' } onClick={ onClick } style={ { filter: count > 0 ? 'grayscale(0)' : 'grayscale(1) brightness(0.2) opacity(0.5)'} }>
-            <span>💪</span><span className={'counter'}>{ count > 0 && count}</span>
+            <span className={ 'flex-icon' }>💪</span>{ count > 0 && <span className={'counter'}>{ count}</span>}
         </div>
     );
 }
