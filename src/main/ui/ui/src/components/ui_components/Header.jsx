413774a6d2e7c6d2690d98f6dc0b1b388b0ebd33
ErkanChalmers <sanneer@student.chalmers.se>
2022-11-17 00:00:08 +0100

    Celebration header text

--- src/main/ui/ui/src/components/ui_components/Header.jsx
+++ src/main/ui/ui/src/components/ui_components/Header.jsx
@@ -6,11 +6,28 @@ const Header = ({ title }) => {
         <header>
                 <h2 className={ 'header' }>
                     <NavLink to="/general" style={{ color: 'inherit', textDecoration: 'none'}}>
-                       { title }
+                       { title } { getCelebrationText() && <span className={'pulsate'} style={celebrationStyle}>{getCelebrationText()}</span> }
                     </NavLink>
                 </h2>
         </header>
     );
+}
+
+const getCelebrationText = () => {
+    const date = new Date();
+    if (date.getMonth() == 10)
+        return `✨ Happy ${date.getFullYear()-2020} year anniversary! ✨`
+    return "";
+}
+
+
+const celebrationStyle = {
+    fontSize: '0.4em',
+    color: '#ffd200',
+    position: 'absolute',
+    left: '2em',
+    bottom: '1em',
+    filter: 'drop-shadow(0px 0px 2px black) drop-shadow(0px 0px 2px black) drop-shadow(0px 0px 2px black) drop-shadow(0px 0px 2px black)'
 }
 
 export default Header;
