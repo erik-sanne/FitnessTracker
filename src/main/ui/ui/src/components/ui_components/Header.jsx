861a31c819b7e1c14d858c3f2946cacd0b28cfa5
ErkanChalmers <sanneer@student.chalmers.se>
2022-11-19 21:48:01 +0100

    More celebration texts

--- src/main/ui/ui/src/components/ui_components/Header.jsx
+++ src/main/ui/ui/src/components/ui_components/Header.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 
 const Header = ({ title }) => {
+
+    const { text, color } = getCelebrationText();
     return (
         <header>
                 <h2 className={ 'header' }>
                     <NavLink to="/general" style={{ color: 'inherit', textDecoration: 'none'}}>
-                       { title } { getCelebrationText() && <span className={'pulsate'} style={celebrationStyle}>{getCelebrationText()}</span> }
+                       { title } { text && <span className={'pulsate'} style={{...celebrationStyle, color: color}}>{text}</span> }
                     </NavLink>
                 </h2>
         </header>
@@ -15,12 +17,19 @@ const Header = ({ title }) => {
 
 const getCelebrationText = () => {
     const date = new Date();
-    if (date.getMonth() == 10)
-        return `✨ Happy ${date.getFullYear()-2020} year anniversary! ✨`
-    return "";
+    if (date.getMonth() === 10)
+        return { text: `✨ Happy ${date.getFullYear()-2020} year anniversary! ✨`, color: "#ffd200"}
+
+    if (date.getMonth() === 11 && [23,24,25].includes(date.getDate()))
+        return { text: `❄️ Marry christmas! ☃️❄️`, color: "#fff"}
+
+    if (date.getMonth() === 11 && [31].includes(date.getDate()) || date.getMonth() === 0 && [1].includes(date.getDate()))
+        return { text: `🎇 Happy new year! 🎆`, color: "#469DAB"}
+
+    return {text: "", color: ""};
 }
 
-
+//#469DAB
 const celebrationStyle = {
     fontSize: '0.4em',
     color: '#ffd200',
