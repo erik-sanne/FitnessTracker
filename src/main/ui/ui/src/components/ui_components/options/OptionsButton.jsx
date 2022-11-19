6f0587a5e029c4678593986643355a50552ce55a
ErkanChalmers <sanneer@student.chalmers.se>
2022-11-19 22:47:42 +0100

    Close options menu on click outside

--- src/main/ui/ui/src/components/ui_components/options/OptionsButton.jsx
+++ src/main/ui/ui/src/components/ui_components/options/OptionsButton.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEllipsisH} from "@fortawesome/free-solid-svg-icons";
 
@@ -9,8 +9,24 @@ const OptionsButton = ({ style, children }) => {
         setShowOptions(!showOptions)
     }
 
+    const ref = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (e) => {
+            if (ref.current && !ref.current.contains(e.target)) {
+                setShowOptions(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            // Unbind the event listener on clean up
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [ref])
+
     return (
-        <div style={{...style, position: 'absolute'}} onClick={ toggleShow }>
+        <div style={{...style, position: 'absolute'}} onClick={ toggleShow } ref={ref}>
             <FontAwesomeIcon icon={faEllipsisH} style={ optionButtonStyle } />
             { showOptions &&
                 <div style={popupStyle}>
