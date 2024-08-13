0752bd9c1d03403efc9288a3bd52edf936074c33
Erkan <erik.sanne@hotmail.se>
2024-08-14 00:35:45 +0200

    Java 21, Gradle 8.9, Spring-Boot 3.3.2

--- src/main/java/com/ersa/tracker/security/configurations/SecurityConfiguration.java
+++ src/main/java/com/ersa/tracker/security/configurations/SecurityConfiguration.java
@@ -2,16 +2,22 @@ package com.ersa.tracker.security.configurations;
 
 import com.ersa.tracker.models.authentication.User;
 import com.ersa.tracker.services.authentication.AuthenticationService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -32,16 +38,15 @@ public class SecurityConfiguration {
 
     @Bean
     protected SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
-        http.cors();
-        http.headers().frameOptions().sameOrigin();
-        http.csrf().disable();
+        http.cors(Customizer.withDefaults());
+        http.headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.csrf(AbstractHttpConfigurer::disable);
         http.sessionManagement(conf -> conf.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(Customizer.withDefaults());
 
-        http.httpBasic();
-
-        http.exceptionHandling(conf -> conf.authenticationEntryPoint(
-                (request, response, authenticationException) ->
-                        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED)));
+        http.exceptionHandling(conf ->
+                conf.authenticationEntryPoint(new CommenceUnauthorized())
+        );
 
         http.authorizeHttpRequests(conf -> conf
                 .requestMatchers("/authenticate").permitAll()
@@ -79,5 +84,12 @@ public class SecurityConfiguration {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    private static class CommenceUnauthorized implements AuthenticationEntryPoint {
+        @Override
+        public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
     }
 }
