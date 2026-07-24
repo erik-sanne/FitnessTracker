d43ad1ddfd7460458be3368f5df8db76d5d0078d
Erik Sänne <erik.sanne@hotmail.se>
2026-07-24 21:49:55 +0200

    refactor: use className instead of color prop in CircularProgressbar

--- src/main/ui/ui/src/components/ui_components/ArchivedGoal.jsx
+++ src/main/ui/ui/src/components/ui_components/ArchivedGoal.jsx
@@ -46,7 +46,7 @@ const ArchivedGoal = ({ id, type, name, startDate, endDate, progress, target, co
                         <CircularProgressbar 
                             value={ progressPercent } 
                             text={ `${progress.toFixed(2)}/${target}` }
-                            color={ progressPercent < 75 ? "#ffff00" : "rgba(2, 160, 2, 0.7)" }
+                            className={ progressPercent < 75 ? "notreached" : "surplus" }
                         />
                     </div>
                     <div>
